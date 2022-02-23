692e53ba92bc6836eb796d5dddff1d09dac64504
spandan0705 <85344474+spandan0705@users.noreply.github.com>
2022-02-23 19:02:07 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload()
+{
+    song1="bdayaudio.mp3";
+    song2="music.mp3";
+}
+
+function play()
+{
+    song1.play();
+}
+
+function previous()
+{
+    song1.play();
+}
+
+function next()
+{
+    song2.play();
+}
